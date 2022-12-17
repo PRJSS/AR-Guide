b0ad6388dcccab15bc609d41aa127870f6cf22d9
Danila Bernat <danila6545@gmail.com>
2022-12-17 19:15:35 +0100

    feat: setup basic algoruthm to put 3d object into image bounce

---
+++ arguide/arguide/AnimationInfo.swift
@@ -0,0 +1,11 @@
+import Foundation
+import SceneKit
+
+struct AnimationInfo {
+    var startTime: TimeInterval
+    var duration: TimeInterval
+    var initialModelPosition: simd_float3
+    var finalModelPosition: simd_float3
+    var initialModelOrientation: simd_quatf
+    var finalModelOrientation: simd_quatf
+}
